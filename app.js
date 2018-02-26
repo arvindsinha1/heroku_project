e8add813013cc0a84f84134d2d4059f0db62589f
asinhamobiquity <asinha@mobiquityinc.com>
2018-02-26 14:16:23 +0530

    ap

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ const request = require('request');
 const express = require('express');
 var app = express();
 var port = process.env.port || 3000 ;
+console.log(process.env.port, 'port');
 
 app.get('/', (req, res) =>{
   console.log('/ is called');
 });
 
-app.listen(process.env.port);
+app.get('/favicon.ico', (req, res) =>{
+  console.log('/favicon.ico is called');
+});
+
+app.listen(port);
