1b99d05cf5c520797a26e1a676c214e125bd3fa3
asinhamobiquity <asinha@mobiquityinc.com>
2018-02-26 14:05:25 +0530

    .

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log('in app.js');
+const request = require('request');
+
+const express = require('express');
+var app = express();
+
+app.get('/', (req, res) =>{
+  console.log('/ is called');
+});
+
+app.listen(3000);
